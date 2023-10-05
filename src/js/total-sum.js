dda9b2c9d6424027cb194e4ba8c59623ada0d13a
Dmitry Domnin <domnin.dm>
2023-10-06 00:32:23 +0300

    interactivity works 0-30 06-10-2023. fonts were updated, all figures were synchronised

--- src/js/total-sum.js
+++ src/js/total-sum.js
@@ -1,18 +1,29 @@
 function updateTotal (arrOfGoods) {
     let totalPr = 0,
-        totalNum = 0;
+        totalNum = 0,
+        totalOld = 0;
+
         arrOfGoods.forEach((good) => {
             if (good.selected && good.displayed) {
                 totalPr += good.totalPrice;
                     totalNum += good.numberOfGoods;
+                        totalOld += good.totalOldPrice;
             }
         })
+    
+    let totalDiscount = (totalOld - totalPr).toLocaleString();
 
+    totalOld = totalOld.toLocaleString();
     totalPr = totalPr.toLocaleString('ru');
+    totalNum = totalNum.toLocaleString();
 
     document.querySelector('.total-price__value').innerText = `${totalPr}`;
     document.querySelector('.added-products-total__closed-price').innerText = `${totalPr} сом`;
-    document.querySelector('.added-products-total__closed-number').innerText = `${totalNum.toLocaleString('ru')} товаров`;
+    document.querySelector('.added-products-total__closed-number').innerText = `${totalNum} товаров`;
+    document.querySelector('.total-price__details-num').innerText = `${totalNum}`;
+    document.querySelector('.total-price__details-old').innerText = `${totalOld}`;
+    document.querySelector('.total-price__details-discount-value').innerText = `−${totalDiscount}`;
+
 }
 
 function updateTotalOld (arrOfGoods) {
--- src/js/update-item-fields.js
+++ src/js/update-item-fields.js
@@ -1,8 +1,8 @@
 function updateItemField (countFields, currPriceFields, currPriceMobFields, oldPriceFields, oldPriceMobFields, index, prodArr) {
     
-    countFields[index].innerText = `${prodArr[index].numberOfGoods}`;
+    countFields[index].innerText = `${prodArr[index].numberOfGoods} `;
     currPriceFields[index].innerText = `${prodArr[index].totalPrice}`;
-    currPriceMobFields[index].innerText = `${prodArr[index].totalPrice}`;
+    currPriceMobFields[index].innerText = `${prodArr[index].totalPrice} `;
     oldPriceFields[index].innerText = `${prodArr[index].totalOldPrice}`;
     oldPriceMobFields[index].innerText = `${prodArr[index].totalOldPrice}`;
 
